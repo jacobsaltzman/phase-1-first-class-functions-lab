6f854309d48b346a3a54fb24c8cd1fe0892c4460
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-04-29 18:19:11 -0700

    commit tree b707ec20f9fd6ee570615eba7bc7a9a5ce97bc91

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(-2,drivers.length)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier){
+    return function fareMultiplier(fare){
+        return fare * multiplier;
+    }
+}
+
+const fareDoubler = (createFareMultiplier(2));
+
+const fareTripler = (createFareMultiplier(3));
+
+function selectDifferentDrivers(drivers, func){
+    if (func == returnFirstTwoDrivers){
+        return returnFirstTwoDrivers(drivers);
+    }
+        return returnLastTwoDrivers(drivers);
+
+}
